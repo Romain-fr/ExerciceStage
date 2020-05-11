543528724bd5ae62c44c7272b1094596f9f1f89b
Romain <romain.rom1@yahoo.fr>
2020-05-11 19:20:25 +0200

    use puma via procfile

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,30 @@
 class PagesController < ApplicationController
+
+  def index
+  end
+
+  def acknowledgements
+  end
+
+  def create
+    Number.create value: params[:value]
+    redirect_to "/index"
+  end
+
+  def sum
+    @number=0.0
+    Number.all.each do |nb|
+      @number = @number + nb.value
+    end
+    Number.create value: @number
+    redirect_to "/index"
+  end
+
+  def destroy_all
+    Number.all.each do |nb|
+      nb.destroy
+    end
+    redirect_to "/index"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root 'pages#index'
+get 'index' => 'pages#index'
+get 'acknowledgements' => 'pages#acknowledgements'
+
+post 'stage' => 'pages#create'
+patch 'stage' => 'pages#sum'
+delete 'stage' => 'pages#destroy_all'
 
 end
